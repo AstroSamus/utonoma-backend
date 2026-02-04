725284e6f5fb6581cbdd971152ddd368e4a063a3
Adrian Sequeira <adrian.sequeira@outlook.com>
2026-02-04 17:04:21 -0600

    Created types file, it contains all the types for the app

---
+++ src/types.ts
@@ -0,0 +1,24 @@
+export interface VideoMetadata {
+  shortVideoTitle: string;
+  shortVideoDescription: string;
+}
+
+export interface PinataPinJsonPayload {
+  pinataContent: unknown;
+  pinataOptions?: {
+    cidVersion?: number;
+  };
+  pinataMetadata?: {
+    name?: string;
+  };
+}
+
+export interface PinataPinJsonResponse {
+  IpfsHash?: string;
+}
+
+export interface CreatePendingUploadParams {
+  content_cid: string;
+  metadata_cid: string;
+  extra_cids: object | null;
+}
