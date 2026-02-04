7fee7e83975281e05e47a392a00ccefb0b401f19
Adrian Sequeira <adrian.sequeira@outlook.com>
2026-02-04 17:29:03 -0600

    Creating type guard for isPinataPinJsonResponse. Helps to validate that the return from the http call for pinning to ipfs is returning correctly

--- src/types.ts
+++ src/types.ts
@@ -17,13 +17,24 @@ export interface PinataPinJsonResponse {
   IpfsHash?: string;
 }
 
+export function isPinataPinJsonResponse(data: unknown): data is PinataPinJsonResponse {
+  if (typeof data !== "object" || data === null) {
+    return false;
+  }
+
+  const d = data as Record<string, unknown>;
+
+  const hasValidCid = typeof d.IpfsHash === "string"
+  return hasValidCid;
+}
+
 export interface CreatePendingUploadParams {
   content_cid: string;
   metadata_cid: string;
   extra_cids: object | null;
 }
 
-type UploadJsonToIpfsInput = 
+export type UploadJsonToIpfsInput = 
   | { //short videos
       type: "json";
       shortVideoTitle: string;
