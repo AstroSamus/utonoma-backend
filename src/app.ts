99b5f7b336a6613cdd04daa2ae6ca62980fa9b56
Adrián Armenta Sequeira <adrian.sequeira@outlook.com>
2026-05-11 16:08:45 -0600

    Parsing body as json using the middleware express.json

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express from 'express'
 import uploadContentRoutes from './routes/uploadContent.routes'
 
 const app = express()
+
+app.use(express.json())
+
 app.use('/upload-content', uploadContentRoutes)
 
 export default app
