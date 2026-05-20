95ad45748ea5262271404cff2b6b9cd92e3be76a
Adrián Armenta Sequeira <adrian.sequeira@outlook.com>
2026-05-20 17:21:40 -0600

    Adding cors to the project and instantiating the middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from 'express'
+import cors from 'cors';
 import uploadContentRoutes from './routes/uploadContent.routes'
 
 const app = express()
 
+
+app.use(cors())
 app.use(express.json())
+
+
 
 app.use('/upload-content', uploadContentRoutes)
 
