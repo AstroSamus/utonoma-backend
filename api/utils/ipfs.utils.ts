525c9d51e6742493546f31136aec38edd4c93035
Adrián Armenta Sequeira <adrian.sequeira@outlook.com>
2026-05-28 18:51:45 -0600

    Created method simulateIpfsCid in the ipfs.utils file. We simulate the CID v0 that we would get from uploading this file to IFPS so we calculate this information without running an IPFS node or without actually uploading the file to IPFS

--- api/utils/ipfs.utils.ts
+++ api/utils/ipfs.utils.ts
@@ -11,6 +11,22 @@ type HashType = {
   ) => Promise<CidV0>
 }
 
+const ipfsOptions = {
+  cidVersion: 0,
+}
+
+export async function simulateIpfsCid(
+  filePath: string
+): Promise<[Error | null, CidV0]> {
+  const stream = createReadStream(filePath)
+  const hash: HashType = Hash as HashType
+  try {
+    const cid = await hash.of(stream, ipfsOptions)
+    return [null, convertIpfsCidToBytes32(cid)]
+  } catch (error) {
+    return [error as Error, '' as CidV0]
+  }
+}
 
 export function convertIpfsCidToBytes32(cid: CidV0) : CidV0 {
   const arrayBase8 = bs58.decode(cid)
