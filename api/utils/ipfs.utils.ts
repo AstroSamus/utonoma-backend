f6767431104e79427adcd885631ce67953ebb886
Adrián Armenta Sequeira <adrian.sequeira@outlook.com>
2026-05-28 18:50:12 -0600

    Created ipfs utils file and the method convertIpfsCidToBytes32. In the Utonoma smart contract we are not storing the cid in plain text, instead we are saving this information as a bytes32, for that we take an IPFS CID v0 and we trim the first two characters (Qm). The remaining part can be fit into bytes32, this method does that compresion.

---
+++ api/utils/ipfs.utils.ts
@@ -0,0 +1,20 @@
+import { createReadStream } from 'fs'
+// @ts-ignore
+import Hash from 'ipfs-only-hash'
+import bs58 from 'bs58'
+
+export type CidV0 = `Qm${string}`
+type HashType = {
+  of: (
+    content: Buffer | NodeJS.ReadableStream,
+    options?: { cidVersion?: number }
+  ) => Promise<CidV0>
+}
+
+
+export function convertIpfsCidToBytes32(cid: CidV0) : CidV0 {
+  const arrayBase8 = bs58.decode(cid)
+  const reducedTo32Bytes = arrayBase8.slice(2)
+  const res = "0x" + Buffer.from(reducedTo32Bytes).toString('hex')
+  return res as CidV0
+}
