9142139fa8a830aeab1163757ce80e67ef2eb9ce
Adrián Armenta Sequeira <adrian.sequeira@outlook.com>
2026-06-01 18:30:53 -0600

    Created new event bus in the infra folder. This module allow us to do a pub-sub for the whole process, were this is running. For now it allow us to send and hear for upload_session_completed events

---
+++ api/infrastructure/eventBus.ts
@@ -0,0 +1,21 @@
+import EventEmmiter from 'events'
+
+export type EventTypesMap = {
+  upload_session_completed: { sessionId: string }
+}
+
+const eventEmmiter = new EventEmmiter()
+
+export const eventBus = {
+  emit: <TEvent extends keyof EventTypesMap> (event: TEvent, payload: EventTypesMap[TEvent]) => {
+      eventEmmiter.emit(event, payload)
+  },
+  on: <TEvent extends keyof EventTypesMap>(
+      event: TEvent, 
+      callback: (payload: EventTypesMap[TEvent]) => void
+  ) => {
+      eventEmmiter.on(event, callback)
+      return () => eventEmmiter.off(event, callback)
+  }
+}
+
